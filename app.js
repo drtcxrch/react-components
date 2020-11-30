a8281e9026d63b374c9dc4d36bec62093c8409a7
Grant Taggart <granttaggartsf@gmail.com>
2020-11-30 10:24:58 -0800

    Complete bare minimum requirements

--- app.js
+++ app.js
@@ -1,1 +1,48 @@
-// TODO
+class GroceryListItem  extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      done: false
+    };
+  }
+
+  onListItemHover() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
+  render() {
+
+    var style = {
+      fontWeight: this.state.done ? 'bold' : 'none'
+    };
+
+    return (
+      <li style={style} onMouseOver={this.onListItemHover.bind(this)}>{this.props.grocery}</li>
+    );
+  }
+};
+
+var GroceryList = (props) => (
+    <ul>
+      {props.groceries.map(grocery =>
+        <GroceryListItem grocery={grocery} />
+        )}
+    </ul>
+);
+
+var App = () => (
+  <div>
+    <h2>Grocery List</h2>
+    <GroceryList groceries={['Beer', 'Coffee']} />
+  </div>
+);
+
+
+
+ReactDOM.render(<App />, document.getElementById("app"));
+
+
